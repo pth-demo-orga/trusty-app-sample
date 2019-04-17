e0f2f4f6b3a536bd1abc68b013ce88b967306d72
Nick Bray <ncbray@google.com>
2019-04-17 16:18:05 -0700

    [libc-test] add basic malloc alignment test
    Bug: 130829217
    Change-Id: Ide6961b897f4967bc45f1828ab60b16f4fa40b8a

--- libc-test/libc_test.c
+++ libc-test/libc_test.c
@@ -222,6 +222,40 @@ TEST_F(libc, malloc_oom) {
 test_abort:;
 }
 
+static uintptr_t expected_malloc_alignment(size) {
+    /* TODO use ffs? */
+    if (size >= 16) {
+        return sizeof(void*) * 2;
+    } else if (size >= 8) {
+        return 8;
+    } else if (size >= 4) {
+        return 4;
+    } else if (size >= 2) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+TEST_F(libc, malloc_alignment) {
+    for (int size = 2; size < 256; size++) {
+        const uintptr_t alignment_mask = expected_malloc_alignment(size) - 1;
+        void* ptr1 = malloc(size);
+        void* ptr2 = malloc(size / 2); /* Try to shake up the alignment. */
+        void* ptr3 = malloc(size);
+
+        ASSERT_EQ(0, (uintptr_t)ptr1 & alignment_mask, "size %d / align %ld",
+                  size, alignment_mask + 1);
+        ASSERT_EQ(0, (uintptr_t)ptr3 & alignment_mask, "size %d / align %ld",
+                  size, alignment_mask + 1);
+
+        free(ptr3);
+        free(ptr2);
+        free(ptr1);
+    }
+test_abort:;
+}
+
 /*
  * Grab the frame pointer in a simple, non-inlined function.
  * Note this isn't a static function. We're trying to game the optimizer and
